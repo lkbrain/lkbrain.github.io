59f62ad6ebd037011fdc15a99e53b7362b79a1c8
Yuxi Qian <166284730+lkbrain@users.noreply.github.com>
2026-06-28 22:15:23 +0800

    Refine homepage affiliation and research field

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Yuxi Qian';
+export const SITE_DESCRIPTION = 'Personal academic website of Yuxi Qian, a 2026 Ph.D. student at USTC and member of MIRA Lab working on AI4EDA, macro placement, global placement, and 3D chip design.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
