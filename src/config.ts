588c343fb42aa63a2205511caa8272b2a4bdb2ca
Yuxi Qian <qianyx20040130@mail.ustc.edu.cn>
2026-06-28 22:02:52 +0800

    Update homepage research introduction

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Yuxi Qian';
-export const SITE_DESCRIPTION = 'Personal academic website of Yuxi Qian, an undergraduate student at USTC and incoming Ph.D. student.';
+export const SITE_DESCRIPTION = 'Personal academic website of Yuxi Qian, a 2026 Ph.D. student at USTC working on AI4EDA, macro placement, global placement, and 3D chip design.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
